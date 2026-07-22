b809bcae5faec417f868478d5441d63dcc1f89fa
saito <birdyfj@gmail.com>
2026-07-23 00:18:20 +0900

    chore: generators exported

---
+++ devpkg/dev.go
@@ -0,0 +1,9 @@
+package devpkg
+
+import (
+	_ "github.com/xoctopus/genx/devpkg/codex"
+	_ "github.com/xoctopus/genx/devpkg/contextx"
+	_ "github.com/xoctopus/genx/devpkg/docx"
+	_ "github.com/xoctopus/genx/devpkg/enumx"
+	_ "github.com/xoctopus/genx/devpkg/uintstr"
+)
